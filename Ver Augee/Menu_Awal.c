16057343c0614cffb3bbebd35a934f908015f8ff
ReihanAugee <47985670+ReihanAugee@users.noreply.github.com>
2020-04-12 09:30:57 +0700

    Add files via upload

---
+++ Ver Augee/Menu_Awal.c
@@ -0,0 +1,88 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<strings.h>
+#include <time.h>
+#include "process.c"
+
+#define MAX 100
+int tampilan_awal(void)
+{
+    int n;
+
+    	printf("                  GAME OF LIFE\n");
+	printf(" =====================================================\n");
+	printf("             1- Tick             \n");
+	printf("             2- Animate          \n");
+	printf("             3- Quit             \n");
+	printf(" =====================================================\n");
+	printf("Enter your choice : "); scanf("%d",&n);
+	return n;
+}
+
+
+int main()
+{
+    int choice,x;
+    int start=0,number,repeat=0;
+    char file[MAX]="";
+
+    while(repeat==0)
+    {
+        printf("Insert file : "); scanf("%s",file);
+        FILE *fp = fopen(file, "r");
+        if (!fp)
+        {
+            printf("Can't open file, Try again \n");
+            repeat=0;
+        }
+        else
+        {
+            readFile(fp);
+            fclose(fp);
+            repeat=1;
+        }
+
+    }
+
+   while (x!=3)
+    {
+        choice = tampilan_awal();
+        switch(choice)
+        {
+            case 1:
+                tick();
+	        break;
+	    case 2:
+            animate();
+		break;
+            case 3:
+                while (start==0)
+                {
+                printf("Insert a new file? \n");
+                printf("1.Yes       2.No \n");
+                printf("Enter your choice : "); scanf("%d",&number);
+                    if(number==1)
+                    {
+                        start=1;
+                        repeat=0;
+                    }
+                    else if (number ==2)
+                    {
+                        printf("Thanks for playing! \n");
+                        start=1;
+                        x=3;
+                    }
+                    else
+                    {
+                        start=0;
+                        printf("Invalid number! Try again \n");
+                    }
+                }
+                break;
+            default:
+	    printf("Invalid number! Try again \n");
+	    break;
+        }
+    }
+}
+
---
+++ Ver Augee/process.c
@@ -0,0 +1,152 @@
+
+char arr[100][100];
+char temp[100][100];
+int kolom,baris;
+
+void readFile(FILE*fp){
+    
+    char str[50];
+    char c;
+
+    // Baca kolom & baris
+    fgets (str, 50, fp);
+    baris=atoi(str);
+    fgets (str, 50, fp);
+    kolom=atoi(str);
+
+    /* Read data*/
+    for(int i=0;i<baris;i++){
+        for(int j=0;j<kolom+1;j++){
+            c=fgetc(fp);
+            arr[i][j]= c;
+            //printf("%c",c);
+        }
+    }
+}
+
+int lifeCheckTest(char temp, int baris, int kolom){
+    if (temp == 'X'){
+        printf("(%d,%d) Found \n",baris,kolom);
+        return 1;
+    }else{
+        printf("(%d,%d)\n",baris,kolom);
+        return 0;
+    }
+}
+int lifeCheck(char temp){
+    if (temp == 'X'){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int hitungTetangga(int loc_r, int loc_c){
+    int total = 0;
+    int i,j,A,B;
+
+    for(i=loc_r-1; i<=loc_r+1; i++){
+        for(j = loc_c-1; j <= loc_c+1; j++){
+            if(i != loc_r || j != loc_c){
+                A = i; B= j;
+                if(i < 0){
+                    A = baris-1;
+                }
+                if(i > baris-1){
+                    A = 0;
+                }
+                if(j < 0){
+                    B = kolom-1;
+                }
+                if(j > kolom-1){
+                    B = 0;
+                }
+                total += lifeCheck(arr[A][B]);
+            }    
+        }
+    }
+    return total;
+}
+
+void copyTemp(){
+    int i,j;
+    for(int i=0;i<baris;i++){
+        for(int j=0;j<kolom+1;j++){
+            arr[i][j]=temp[i][j];
+        }
+    }
+}
+
+
+void nextGen(){
+    int i,j,sumTetangga = 0;
+    for(i = 0; i< baris; i++){
+        for(j= 0; j < kolom+1; j++){
+            sumTetangga = hitungTetangga(i,j);
+            temp[i][j]=arr[i][j];
+            if (lifeCheck(arr[i][j])){
+                if(sumTetangga <= 1){
+                    temp[i][j] = '-'; //DIE
+                } else if (sumTetangga >= 4){
+                    temp[i][j] = '-'; //DIE
+                }
+            } else if (!lifeCheck(arr[i][j])){
+                if (sumTetangga == 3){
+                    temp[i][j] = 'X'; //Life
+                }
+            }
+        }
+    }
+    copyTemp();
+}
+
+
+
+void printArr(){
+    int i,j;
+    for(i = 0; i<=baris; i++){
+        for(j= 0; j <= kolom+1; j++){
+            printf("%c",arr[i][j]);
+        }
+    }
+}
+
+void tick(){
+    nextGen();
+    printArr();
+}
+
+
+void delay(int milli_seconds) 
+{ 
+ 
+    clock_t start_time = clock(); 
+  
+    while (clock() < start_time + milli_seconds)
+    ; 
+} 
+
+void animate(){
+    int i;
+    system("clear");
+    delay(10000); 
+    for (i =0; i <30; i++){
+        system("clear"); 
+        tick();
+        delay(1000);
+    }
+}
+
+/*
+int main(){
+    int A = 2;
+    int B = 6;
+    readFile("glider-gun.txt");
+    printArr();
+    //printf("Test = %c\n",arr[30][44]);
+    nextGen();
+    printf("\n\n");
+    printArr();
+    return 0;
+}
+*/
